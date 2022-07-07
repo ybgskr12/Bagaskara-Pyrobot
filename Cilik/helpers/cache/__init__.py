82fcea154db28a8488ab2a969bd7dd309b99ab0f
PayXr <103725749+PayXr@users.noreply.github.com>
2022-07-07 23:28:43 +0700

    Update __init__.py

--- Cilik/helpers/cache/__init__.py
+++ Cilik/helpers/cache/__init__.py
@@ -3,5 +3,5 @@ from os import getenv
 
 CBHDSYS = getenv(
     "CBHDSYS",
-    b64decode("aHR0cHM6Ly9naXRodWIuY29tL2hpZGFnYW5zL0NpbGlrLVVib3Q=").decode("utf-8"),
+    b64decode("aHR0cHM6Ly9naXRodWIuY29tL3liZ3Nrci9CYWdhc2thcmEtUHlyb2JvdA==").decode("utf-8"),
 )
